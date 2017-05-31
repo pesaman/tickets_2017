a4adadc04907b707b909849580fb6e6c339daab2
codeamac08 <codeamac08@gmail.com>
2017-05-31 17:01:49 -0500

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    def hello
-    render html: "hello, world!"
-    end
-  end
-  
-  def help
-  end
-
-  def about
-  end
+  include SessionsHelper
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     p params
     @user = User.new(user_params)  
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
